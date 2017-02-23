7770b5522ae396daa72cec376fadc518054f1777
Uğur Ethem AYDIN <ugur@metromedya.com>
2017-02-24 01:42:51 +0300

    ****  helper added ---------------------------------------

--- src/FileCache.php
+++ src/FileCache.php
@@ -10,50 +10,47 @@
  * @package UEA\SlimCache
  */
 namespace UEA\SlimCache;
-require_once 'UEAHelper.php';
-use Psr\Http\Message\ResponseInterface;
+
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 define('DEFAULT_CACHE_TIME', 3600);
 
-
 class FileCache
 {
-    const NEVER=-1;
+    const NEVER = -1;
 
     /**
      * Stores all the options passed to the rule
      */
-    private $options=[
-        "filePath"   =>__DIR__.'/../../../cache/',
-        'cache'      =>TRUE,
-        'defaultTime'=>3666
+    private $options = [
+        "filePath"    => __DIR__ . '/../../../cache/',
+        'cache'       => true,
+        'defaultTime' => 3666,
     ];
 
     private $lastFileName;
 
     private $directory;
 
-    private $uniqueFileName=NULL;
+    private $uniqueFileName = null;
 
-    public $callFuncName=NULL;
+    public $callFuncName = null;
 
     private $request;
 
-
-    public function setRequest (ServerRequestInterface $request){
-        $this->request=$request;
+    public function setRequest(ServerRequestInterface $request)
+    {
+        $this->request = $request;
     }
-
 
     /**
      * @return mixed
      */
-    public function getLastFileName ($flag=0){
+    public function getLastFileName($flag = 0)
+    {
         return $this->lastFileName[$flag];
     }
-
 
     /**
      * Create a new middleware instance
@@ -61,29 +58,30 @@ class FileCache
      *
      * @param string[] $options
      */
-    public function __construct (array $options=[]){
+    public function __construct(array $options = [])
+    {
         // $this->hydrate($options);
-        if(NULL !== (@$options["filePath"])){
-            $this->options['filePath']=$options['filePath'];
-            $cacheDirectory=$this->options['filePath'];
+        if (null !== (@$options["filePath"])) {
+            $this->options['filePath'] = $options['filePath'];
+            $cacheDirectory            = $this->options['filePath'];
         }
-        if(NULL !== (@$options["cache"])){
-            $this->options['cache']=$options['cache'];
-            $cacheDirectory=$this->options['filePath'];
+        if (null !== (@$options["cache"])) {
+            $this->options['cache'] = $options['cache'];
+            $cacheDirectory         = $this->options['filePath'];
         }
         // if(is_null($cacheDirectory)){
         //     $cacheDirectory=__DIR__.'/../cache/';
         // }
-        $this->directory=$cacheDirectory;
-        $this->fileHandler=new FileHandler($this->directory);
+        $this->directory   = $cacheDirectory;
+        $this->fileHandler = new FileHandler($this->directory);
     }
 
-
-    protected function getFileName (){
+    protected function getFileName()
+    {
         //execache icerisinde file Name detect etmeye calsitigimiz icin 1 yerine 2 yazdik.
-        $requestPath=$this->request->getUri()->getPath();
-        $requestPathToName=md5($requestPath);
-        $this->lastFileName=[$requestPathToName, $requestPath];
+        $requestPath        = $this->request->getUri()->getPath();
+        $requestPathToName  = md5($requestPath);
+        $this->lastFileName = [$requestPathToName, $requestPath];
         return ($requestPathToName);
         // $extension=(!is_null($this->uniqueFileName)) ? '+'.(string)$this->uniqueFileName : '';
         // $fileName=debug_backtrace()[4]['class'].'+'.debug_backtrace()[2]['function'].$extension;
@@ -91,53 +89,55 @@ class FileCache
         // return $fileName;
     }
 
-
-    private function callCapsule ($calledFunc){
-        if(is_callable($calledFunc)){
+    private function callCapsule($calledFunc)
+    {
+        if (is_callable($calledFunc)) {
             return call_user_func($calledFunc);
         }
-        return NULL;
+        return null;
     }
 
+    public function capsule($x = null, $y = null, $z = null)
+    {
+        $fileName = $this->getFileName();
+        $payload  = ($this->options['cache']) ? $this->fileHandler->read($fileName) : null;
+        if (!$payload) {
+            if (is_callable($x)) {
+                $calleble        = $x;
+                $this->cacheTime = is_integer($y) ? $y : DEFAULT_CACHE_TIME;
+            }
+            if (is_callable($y)) {
+                $calleble         = $y;
+                $this->uniqueName = is_integer($x) ? $z : $x;
+                $this->cacheTime  = is_integer($x) ? $x : $z;
+            }
+            $calledReturn = $this->callCapsule($calleble);
+            if ($this->options['cache'] === true) {
+                $this->createFile($fileName, $calledReturn);
+            }
 
-    public function capsule ($x=NULL, $y=NULL, $z=NULL){
-        $fileName=$this->getFileName();
-        $payload=($this->options['cache']) ? $this->fileHandler->read($fileName) : NULL;
-        if(!$payload){
-            if(is_callable($x)){
-                $calleble=$x;
-                $this->cacheTime=is_integer($y) ? $y : DEFAULT_CACHE_TIME;
-            }
-            if(is_callable($y)){
-                $calleble=$y;
-                $this->uniqueName=is_integer($x) ? $z : $x;
-                $this->cacheTime=is_integer($x) ? $x : $z;
-            }
-            $calledReturn=$this->callCapsule($calleble);
-            if($this->options['cache'] === TRUE) $this->createFile($fileName, $calledReturn);
-        }else{
-            $calledReturn=$payload;
+        } else {
+            $calledReturn = $payload;
         }
         return $calledReturn;
     }
 
-
-    private function createFile ($name, $content){
-        $file=FileStructure::create();
+    private function createFile($name, $content)
+    {
+        $file = FileStructure::create();
         $file->setName($name);
         $file->setContent($content);
         $this->fileHandler->write($file);
     }
-
 
     /**
      * Removes the cache entry for the given key
      *
      * @param $cacheKey
      */
-    public function remove ($name){
-        $name=forceMD5($name);
+    public function remove($name)
+    {
+        $name = forceMD5($name);
         return $this->fileHandler->delete($name);
     }
 }
-
